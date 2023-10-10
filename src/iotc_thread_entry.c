2b7f0030d04bf3da7d5173e196f9316638a68acd
Brian Willess <41625691+bawilless@users.noreply.github.com>
2023-10-10 10:14:11 -0400

    Correct BLE functionality
    Allow the iotc thread to run if BLE mode is enabled.

--- src/iotc_thread_entry.c
+++ src/iotc_thread_entry.c
@@ -72,14 +72,6 @@ void iotc_thread_entry(void *pvParameters)
     // Put the DA16600 into reset
     R_BSP_PinWrite(DA16600_RstPin, BSP_IO_LEVEL_LOW);
 
-    // If we only need BLE mode and the BLE name is the default, just take the DA16600 out of reset and exit this thread
-    if((CLOUD_NONE == get_target_cloud()) && (BLE_ENABLE == get_ble_mode()) && (0 == strncmp(BLE_DEFAULT_NAME, get_ble_name(), 32))){
-
-        // Pull the DA16600 out of reset and kill this task
-        R_BSP_PinWrite(DA16600_RstPin, BSP_IO_LEVEL_HIGH);
-        vTaskDelete(NULL);
-    }
-
     // If we don't need the DA16600 at all then leave the device in reset and kill this task.
     if(((CLOUD_NONE == get_target_cloud()) && (BLE_DISABLE == get_ble_mode()))){
 
