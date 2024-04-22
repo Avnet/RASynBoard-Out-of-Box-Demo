0e9add329b03ffb5ebbe18b36604ee9d6d3ebb1e
Brian W <brian.willess@avnet.com>
2024-04-22 07:12:05 -0500

    Correct data conversons and data labels for IMU data capture

--- src/ndp_record_thread_entry.c
+++ src/ndp_record_thread_entry.c
@@ -133,12 +133,15 @@ void icm42670_extraction_cb(uint32_t sample_size, uint8_t *sensor_data, void *se
 
         for (j = 0; j < (sample_size/INERTIAL_AXIS_SAMPLED); j++) {
             for (i = 0; i < 3; i++) {
-                acc_converted_samples[(j * INERTIAL_AXIS_SAMPLED) + i] = acc_samples[(j * INERTIAL_AXIS_SAMPLED) + i] * ACC_SCALE_FACTOR;
 
+                // Update the converted samples array with converted gyro data
+                acc_converted_samples[(j * INERTIAL_AXIS_SAMPLED) + i] = acc_samples[(j * INERTIAL_AXIS_SAMPLED) + i] * CONVERT_ADC_GYR;
             }
 
             for (i = 3; i < INERTIAL_AXIS_SAMPLED; i++) {
-                acc_converted_samples[(j * INERTIAL_AXIS_SAMPLED) + i] = acc_samples[(j * INERTIAL_AXIS_SAMPLED) + i] * CONVERT_ADC_GYR;
+
+                // Update the converted samples array with converted accelerometer data
+                acc_converted_samples[(j * INERTIAL_AXIS_SAMPLED) + i] = acc_samples[(j * INERTIAL_AXIS_SAMPLED) + i] * ACC_SCALE_FACTOR;
             }
         }
 
